69598385dda64d78c8024d3b0bc4c0cda8843d98
ococa <wangchao.personal@gmail.com>
2020-08-05 17:34:03 +0800

    feat: priority queue

--- src/Queue/PriorityQueue.java
+++ src/Queue/PriorityQueue.java
@@ -1,33 +1,37 @@
 package Queue;
 
-public class PriorityQueue<E> implements Queue<E> {
+import MaxHeap.MaxHeap;
+
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+
+    private MaxHeap<E> maxHeap;
 
     public void PriorityQueue() {
-
+        maxHeap = new MaxHeap<E>();
     }
 
     @Override
     public void enqueue(E e) {
-
+        maxHeap.add(e);
     }
 
     @Override
     public E dequeue() {
-        return null;
+        return maxHeap.extractMax();
     }
 
     @Override
     public E getFront() {
-        return null;
+        return maxHeap.findMax();
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return maxHeap.getSize();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return maxHeap.isEmpty();
     }
 }
